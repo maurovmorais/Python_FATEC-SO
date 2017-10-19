79cdab303fc74bc9dea72888740e382241340436
Mauro Vieira de Morais <maurovmorais@gmail.com>
2017-10-19 17:33:15 -0200

    Solução Enigma TOTVS

---
+++ Enigma.py
@@ -0,0 +1,17 @@
+# Decifra Enigma.
+dec = 0
+e = []
+
+while True :
+	bin = input("Digite o binário ou \"c\" para corrigir ou \"f\" finalizar: ")
+	if bin == "f":
+		print("Terminou !")
+		break
+	if bin == "c":
+		e.pop(-1)
+		print("Enigma: ",''.join(e))
+		continue
+	dec = int(bin,2)
+	letra = chr(dec)
+	e.append(letra)
+	print("Enigma: ",''.join(e))
