4e45698d57a0fc7ad9088fd16b408f55253add77
Mauro Vieira de Morais <maurovmorais@gmail.com>
2017-10-22 19:00:39 -0200

    Cálculo MDC

---
+++ mdc.py
@@ -0,0 +1,10 @@
+def mdc(a,b):
+    if b == 0:
+        return a
+    return mdc(b, a % b)
+
+a = int(input("Digite numero 1: "))
+b = int(input("Digite numero 2: "))
+
+print("MDC : %d" % mdc(a,b))
+print("Teste %d" % mdc(20,50))
